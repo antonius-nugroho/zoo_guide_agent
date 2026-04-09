6c6fec2a3b3b6146ef2be9dd83fabf67840a3801
Antonius Nugroho <150353288+antonius-nugroho@users.noreply.github.com>
2026-04-09 09:15:45 +0700

    Add files via upload

---
+++ zoo_database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE animals (
+   species STRING,
+   name STRING,
+   age INT64,
+   location STRING
+);
+
+INSERT INTO animals (species, name , age, location)
+VALUES 
+   ('lion', 'Leo', 12, 'Savanna'),
+   ('penguin', 'Penny', 3, 'Arctic Zone'),
+   ('sea lion', 'Sely', 3, 'Arctic Zone'),
+   ('tiger', 'Loreng', 5, 'Savanna'),
+   ('monkey', 'Kiky', 4, 'Woods'),
+   ('giraffe', 'Gigi', 4, 'Savanna'),
+   ('dog', 'Doug', 6, 'Home Pets'),
+   ('dog', 'Digg', 4, 'Home Pets');
